1534097b087e80b6d4d90ce091c006b684022c2d
Di Voz <40432724+dianavoz@users.noreply.github.com>
2020-09-09 10:54:46 +0100

    Update courses.js

--- routes/courses.js
+++ routes/courses.js
@@ -1,148 +1,205 @@
+'use strict';
+
 const express = require('express');
+const {Course, User} = require('../models');
+const authenticateUser = require("./authenticate");
+const { check, validationResult } = require("express-validator");
+
 const router = express.Router();
 
-const {Course, User} = require('../models');
-const authenticateUser = require("./authenticateUser");
-
-const { check, validationResult } = require("express-validator");
 
 // Helper function so that we don't need to add try/catch to every route
 function asyncHandler(cb) {
-    return async (req, res, next) => {
-      try {
-        await cb(req, res, next);
-      } catch (err) {
-        next(err);
-      }
-    };
-  }
-
-// GET all courses
-router.get('/', asyncHandler(async (req, res) => {
-      // Finds a lists all courses on database
-      const courses = await Course.findAll({
-          include: [{
-              model: User,
-              attributes: {
-                  exclude: ['password', 'createdAt', 'updatedAt']
-              }
-          }],
-          attributes: {
-              exclude: ['userId', 'createdAt', 'updatedAt']
-          }
-      })
-
-      res.status(200).json({ courses });
-}));
-
-
-//GET the course by ID
-router.get('/:id', asyncHandler(async(req, res) => {
-    const course = await Course.findByPk(req.params.id, {
-        attributes: ["id","title", "description","userId"],
-        include:[{
-            model: User,
-            attributes: ["id","firstName","lastName","emailAddress"]
-        }]
-    });
-
-    if(course){
-        res.status(200).json({course});
-    }else{
-        res.status(400).json({message: 'course not found!'});
+  return async (req, res, next) => {
+    try {
+      await cb(req, res, next);
+    } catch (err) {
+      next(err);
     }
+  };
+}
+
+
+// GET /api/courses 200
+// Returns a list of courses (including the user that owns each course)
+router.get('/', asyncHandler( async (req, res) => {
+	const courses = await Course.findAll({
+		attributes: ["id", "title", "description", "userId"],
+		include: [
+			{
+				model: User,
+				attributes: ["id", "firstName", "lastName", "emailAddress",]
+			}
+		]
+	});
+	res.json({ courses });
 }));
 
+
+// Returns a course (including the user that owns the course) for the provided course ID
+router.get('/:id', asyncHandler( async (req, res) => {
+	const course = await Course.findByPk(req.params.id, {
+		attributes: ["id", "title", "description", "userId"],
+		include: [
+			{
+				model: User,
+				attributes: ["id", "firstName", "lastName", "emailAddress"]
+			}
+		]
+	});
+	
+	if (course) {
+		res.json({ course });
+	} else {
+		res.status(404).json({ message: 'Course id not found.' });
+	}
+}));
 
 
 // Creates a course, sets the Location header to the URI for the course, and returns no content
-router.post('/',authenticateUser, async (req, res) => {
-    try {
-        const user = req.currentUser;
-        const newCourse = req.body;
-        let duplicateCheck = null;
-        
-        if (newCourse.title) { 
-            // Checks if there is a course w/ this title
-            duplicateCheck = await Course.findOne({
-                where: {
-                    title: newCourse.title
-                }
-            });
-        }
+router.post('/', [
+	check('title')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "title"'),
+	check('description')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "description"'),
+],
+authenticateUser, asyncHandler( async (req, res) => {
+	// Attempt to get the validation result from the Request object.
+	const errors = validationResult(req);
 
-        if (!duplicateCheck) {
-            // If not Null: make new course
-            await Course.create({
-                title: newCourse.title,
-                description: newCourse.description,
-                estimatedTime: newCourse.estimatedTime,
-                materialsNeeded: newCourse.materialsNeeded,
-                userId: user.id
-            });
+	// If there are validation errors...
+	if (!errors.isEmpty()) {
+		// Use the Array `map()` method to get a list of error messages.
+		const errorMessages = errors.array().map(error => error.msg);
+		// Return the validation errors to the client.
+		return res.status(400).json({ errors: errorMessages });
+	} else {
 
-            const courseUri = await Course.findOne({
-                where: {
-                    title: newCourse.title
-                },
-                attributes: {
-                    include: ['id']
-                }
-            });
-            
-            res.status(201).location(`/courses/${courseUri.dataValues.id}`).end();
-        } else {
-            // If null: gives message
-            res.status(400).json({
-                errors: {
-                    errors: {
-                        message: 'There is a course with that title already.'
-                    }
-                }
-            });
-        }
-        
-    } catch (err) {
-        res.status(400).json({ errors: err });
-    }
-});
+		// get the user from the request body.
+		const course = req.body;
+
+		// Create user
+		const addedCourse = await Course.create({
+      title: course.title,
+      description: course.description,
+      userId: req.currentUser.id
+    });
+
+		// get new course id for Location header
+		const id = addedCourse.id;
+
+		// Set the status to 201 Created, set Location header, and end the response.
+		res.location(`/api/courses/${id}`).status(201).end();
+	}
+}));
 
 
+// PUT /api/courses/:id 204
 // Updates a course and returns no content
-router.put('/:id', authenticateUser, asyncHandler((req, res, next) => {
-    if (req.course) {
-        let { course, body, currentUser } = req;
-    
-        if (course.user.id == currentUser.id) {
-          course.updateOne(body, (err) => {
-            if(err) return next(err);
-            res.status(204).end();
-          });
-        } else {
-          res.status(403).json({ message: 'Access denied' });
-        }
-      } else {
-        res.status(404).json({ message: 'Course not found' });
-      }
-  
-}));
+router.put('/:id', [
+	check('title')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "title"'),
+	check('description')
+		.exists({ checkNull: true, checkFalsy: true })
+		.withMessage('Please provide a value for "description"'),
+	],
+	authenticateUser,  asyncHandler( async (req, res) => {
+		// Attempt to get the validation result from the Request object.
+		const errors = validationResult(req);
 
+		// If there are validation errors...
+		if (!errors.isEmpty()) {
+			// Use the Array `map()` method to get a list of error messages.
+			const errorMessages = errors.array().map(error => error.msg);
+			// Return the validation errors to the client.
+			return res.status(400).json({ errors: errorMessages });
+		} else {
+
+			// find existing course
+			const course = await Course.findByPk(req.params.id, {
+				attributes: ["id", "title", "description", "userId"],
+				include: [
+					{
+						model: User,
+						attributes: ["id", "firstName", "lastName", "emailAddress"]
+					}
+				]
+			});
+
+			// if course exists
+			if (course) {
+				// if course owner matches current user
+				if (course.userId == req.currentUser.id) {
+					// update course details in Courses table
+					const updatedCourse = await Course.update({
+						title: req.body.title,
+						description: req.body.description
+					}, {
+						where: {
+							id: course.id
+						}
+					});
+
+					if (updatedCourse) {
+						res.status(204).end();
+					}
+
+				} else {
+					// Return a response with a 403 Client forbidden HTTP status code.
+        	res.status(403).json({ message: "Access not permitted" });
+				}
+			} else {
+				res.status(404).json({ message: "Course not found." });
+			}
+		}
+	}
+));
+
+
+// DELETE /api/courses/:id 204
 // Deletes a course and returns no content
-router.delete('/:id', authenticateUser, asyncHandler((req, res) => {
-    if (req.course) {
-        const { course, currentUser } = req;
-    
-        if (course.user.id == currentUser.id) {
-          course.remove((err) => {
-            if(err) return next(err);
-            res.status(204).end();
-          });
-        } else {
-          res.status(403).json({ message: 'Access denied' });
-        }
+router.delete('/:id', authenticateUser, asyncHandler(async (req, res) => {
+
+      // find existing course
+      const course = await Course.findByPk(req.params.id, {
+        attributes: ["id", "title", "description", "userId"],
+        include: [
+          {
+            model: User,
+            attributes: ["id", "firstName", "lastName", "emailAddress"]
+          }
+        ]
+      });
+
+			// if course exists
+      if (course) {
+				// if course owner matches current user
+				if (course.userId == req.currentUser.id) {
+					// delete course from Courses table
+					const deletedCourse = await Course.destroy(
+						{
+							where: {
+								id: course.id
+							}
+						}
+					);
+
+					if (deletedCourse) {
+						res.status(204).end();
+					}
+				
+				} else {
+					// Return a response with a 403 Client forbidden HTTP status code.
+        	res.status(403).json({ message: "Access not permitted" });
+				}
       } else {
-        res.status(404).json({ message: 'Course not found' });
+        res.status(404).json({ message: "Course not found." });
       }
-}));
+  })
+);
+
 
 module.exports = router;
